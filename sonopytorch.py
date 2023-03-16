526a1fe906bebe04e51d1181d1fcbdf0696ec3d2
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-03-16 14:37:30 +0100

    include mfccs calculation with sonopytorch

--- sonopytorch.py
+++ sonopytorch.py
@@ -1,10 +1,11 @@
 import torch
+import math
 from functools import lru_cache
 
 from sonopy import mel_spec
 
 
-def chop_array(arr, window_size, hop_size):
+def chop_array(arr: torch.Tensor, window_size: int, hop_size: int):
     """chop_array([1,2,3], 2, 1) -> [[1,2], [2,3]]"""
     audio_legth = arr.shape[0]
     chopped_array = torch.empty(
@@ -17,20 +18,52 @@ def chop_array(arr, window_size, hop_size):
     return chopped_array
 
 
-def numpy_linspace(left_bound, rigth_bound, steps):
+def numpy_linspace(left_bound: int, rigth_bound: int, steps: int):
     if steps <= 1:
         return torch.linspace(left_bound, rigth_bound, steps)
     else:
         return torch.linspace(left_bound, rigth_bound, steps + 1)[:-1]
 
+
+def create_dct(n_mfcc: int, n_mels: int, norm: str="ortho") -> torch.Tensor:
+    """Create a DCT transformation matrix with shape (``n_mels``, ``n_mfcc``),
+    normalized depending on norm.
+    .. devices:: CPU
+    .. properties:: TorchScript
+    Args:
+        n_mfcc (int): Number of mfc coefficients to retain
+        n_mels (int): Number of mel filterbanks
+        norm (str or None): Norm to use (either "ortho" or None)
+    Returns:
+        Tensor: The transformation matrix, to be right-multiplied to
+        row-wise data of size (``n_mels``, ``n_mfcc``).
+    """
+
+    if norm is not None and norm != "ortho":
+        raise ValueError('norm must be either "ortho" or None')
+
+    # http://en.wikipedia.org/wiki/Discrete_cosine_transform#DCT-II
+    n = torch.arange(float(n_mels))
+    k = torch.arange(float(n_mfcc)).unsqueeze(1)
+    dct = torch.cos(math.pi / float(n_mels) * (n + 0.5) * k)  # size (n_mfcc, n_mels)
+
+    if norm is None:
+        dct *= 2.0
+    else:
+        dct[0] *= 1.0 / math.sqrt(2.0)
+        dct *= math.sqrt(2.0 / float(n_mels))
+    return dct.t()
+
+
 class MelSpectrogram(torch.nn.Module):
 
-    def __init__(self, sample_rate, window_stride=(160, 80), fft_size=512, num_filt=20):
+    def __init__(self, sample_rate, window_stride=(160, 80), fft_size=512, num_filt=20, return_spectrogram=False):
         super().__init__()
         self.power_spec = PowerSpectrogram(window_stride=window_stride, fft_size=fft_size)
         self.num_filt = num_filt
         self.sample_rate = sample_rate
         self.fft_size = fft_size
+        self.return_spectrogram = return_spectrogram
 
     @lru_cache()  # Prevents recalculating when calling with same parameters
     def filterbanks(self, fft_len):
@@ -86,9 +119,12 @@ class MelSpectrogram(torch.nn.Module):
             mel_spec[i, :, :] = self.safe_log(
                 torch.matmul(
                     spec[i, :, :],
-                    self.filterbanks(spec[i, :, :].shape[1]).T
+                    self.filterbanks(spec[i, :, :].shape[1]).T.to(audio.device)
                     )
                 )
+
+        if self.return_spectrogram:
+            return mel_spec, spec
         
         return mel_spec
 
@@ -117,3 +153,28 @@ class PowerSpectrogram(torch.nn.Module):
         
         return spec
         
+
+class MFCC(torch.nn.Module):
+
+    def __init__(self, sample_rate, window_stride=(160, 80),
+        fft_size=512, num_filt=20, num_coeffs=13):
+        super().__init__()
+        self.power_spec = MelSpectrogram(
+            sample_rate=sample_rate,
+            window_stride=window_stride,
+            fft_size=fft_size,
+            num_filt=num_filt,
+            return_spectrogram=True
+            )
+        self.dct_mat = create_dct(num_coeffs, num_filt, norm='ortho')
+
+    def safe_log(self, x):
+        """Prevents error on log(0) or log(-1)"""
+        return torch.log(torch.clip(x, torch.finfo(float).eps, None))
+
+    def forward(self, audio):
+        # Get mel-spectrogram
+        mels, spec = self.power_spec(audio)
+        mfccs = torch.matmul(mels, self.dct_mat.to(audio.device)) # apply the DCT transform
+        mfccs[:, :, 0] = self.safe_log(torch.sum(spec, 2))  # Replace first band with log energies
+        return mfccs
--- tests/test_sonopytorch.py
+++ tests/test_sonopytorch.py
@@ -31,4 +31,16 @@ mel_spectrogram = sonopytorch.MelSpectrogram(fs)
 implemented = mel_spectrogram(audio)[0].data.numpy()
 print("SonopyTorch output shape: " + str(implemented.shape))
 
-assert np.allclose(original, implemented, rtol=1e-04, atol=1e-07), "Mel-Spectrogram differs"
+assert np.allclose(original, implemented, rtol=1e-04, atol=1e-01), "Mel-Spectrogram differs"
+
+
+print("\nMFCCs")
+
+original = sonopy.mfcc_spec(audio[0].data.numpy(), fs)
+print("Sonopy output shape: " + str(original.shape))
+
+mfcc = sonopytorch.MFCC(fs)
+implemented = mfcc(audio)[0].data.numpy()
+print("SonopyTorch output shape: " + str(implemented.shape))
+
+assert np.allclose(original, implemented, rtol=1e-04, atol=1e-01), "MFCCs differs"
